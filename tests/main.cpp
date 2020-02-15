55ae0c4aca588b4034a1f6597011d303928622c2
ukabuer <ukabuer@live.com>
2020-02-15 23:20:32 +0800

    initial commit

---
+++ tests/main.cpp
@@ -0,0 +1,57 @@
+#include <seria/formatter.hpp>
+#define CATCH_CONFIG_MAIN
+#include "third_party/catch.hpp"
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
+using namespace std;
+
+struct Inside {
+  int i_age = 1;
+  float i_value = 0.111;
+  std::vector<int> i_v = {1, 2, 3, 4, 5};
+};
+
+struct Person {
+  int age = 0;
+  float value = 3.333;
+  Inside inside{};
+};
+
+namespace seria {
+
+template <> auto registerObject<Person>() {
+  return std::make_tuple(member("age", &Person::age),
+                         member("value", &Person::value),
+                         member("inside", &Person::inside));
+}
+
+template <> auto registerObject<Inside>() {
+  return std::make_tuple(member("i_age", &Inside::i_age),
+                         member("i_value", &Inside::i_value),
+                         member("i_v", &Inside::i_v));
+}
+
+} // namespace seria
+
+TEST_CASE("register", "") {
+  Person person{100, 1.2345};
+  {
+    auto res = seria::serialize(person);
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    res.Accept(writer);
+    cout << buffer.GetString() << endl;
+  }
+
+  const char *str =
+      R"({"age":0,"value":233.0,"inside":{"i_age":233,"i_value":0.233,"i_v":[6,66,666]}})";
+
+  rapidjson::Document document;
+  document.Parse(str);
+  seria::deserialize(person, document);
+
+  REQUIRE(person.age == 0);
+  REQUIRE(person.value == 233.0f);
+  REQUIRE(person.inside.i_age == 233);
+}
